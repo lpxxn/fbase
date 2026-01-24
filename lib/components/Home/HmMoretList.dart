190985758c30f35adee9931c9fada442adb6a9dd
lpxxn <mi_duo@live.com>
2026-01-24 21:12:36 +0800

    feat: update

--- lib/components/Home/HmMoretList.dart
+++ lib/components/Home/HmMoretList.dart
@@ -23,11 +23,10 @@ class _HmMoretListState extends State<HmMoretList> {
         crossAxisCount: 2,
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
+        childAspectRatio: 0.7,
       ),
       itemBuilder: (BuildContext context, int index) {
         return Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
           // child: Text("商品$index", style: const TextStyle(color: Colors.white)),
           child: _getChildren(index),
         );
@@ -39,9 +38,68 @@ class _HmMoretListState extends State<HmMoretList> {
     return Container(
       child: Column(
         children: [
+          // Expanded(
+          // child: ClipRRect(
           ClipRRect(
             borderRadius: BorderRadius.circular(8),
-            child: Image.network(widget.recommendList[index].picture ?? ""),
+
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Image.network(
+                widget.recommendList[index].picture ?? "",
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Image.asset(
+                    "lib/asset/images/1.jpg",
+                    fit: BoxFit.cover,
+                  );
+                },
+              ),
+            ),
+          ),
+          // ),
+          const SizedBox(height: 6),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Text(
+              widget.recommendList[index].name ?? "",
+              maxLines: 2,
+              style: const TextStyle(fontSize: 20, color: Colors.black87),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "¥${widget.recommendList[index].price ?? ""}",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    children: [
+                      const TextSpan(text: "  "),
+                      TextSpan(
+                        text: "${widget.recommendList[index].price ?? ""}",
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  "${widget.recommendList[index].payCount ?? "0"}人付款",
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
           ),
         ],
       ),
