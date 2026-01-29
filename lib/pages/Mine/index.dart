894bdb8a90355ac20b58800d2f5ae46ac3238f6c
lpxxn <mi_duo@live.com>
2026-01-29 22:32:16 +0800

    feat: new feature

--- lib/pages/Mine/index.dart
+++ lib/pages/Mine/index.dart
@@ -8,8 +8,57 @@ class MineView extends StatefulWidget {
 }
 
 class _MineViewState extends State<MineView> {
+  Widget _getLogout() {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("确认退出登录吗？"),
+              content: const Text("确认退出登录吗？"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("取消"),
+                ),
+                TextButton(onPressed: () {}, child: const Text("确认")),
+              ],
+            );
+          },
+        );
+      },
+      child: const Text("退出"),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFFFFF2E8), Color(0xFFFDF6F1)],
+        ),
+      ),
+      padding: const EdgeInsets.only(left: 20, right: 40, top: 80, bottom: 20),
+      child: const Row(
+        children: [
+          CircleAvatar(
+            radius: 26,
+            backgroundImage: AssetImage("lib/asset/images/goods_avatar.png"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("我的")));
+    return CustomScrollView(
+      slivers: [SliverToBoxAdapter(child: _buildHeader())],
+    );
   }
 }
